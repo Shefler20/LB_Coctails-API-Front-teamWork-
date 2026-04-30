a5f581cd51587c7900967bc6e65c4ff5a413ed9d
Ular <eldarakashka@gmail.com>
2026-04-30 15:41:09 +0600

    замержил конфликт между дев и KAN_12

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,25 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import { Provider } from "react-redux";
 import {CssBaseline} from "@mui/material";
 import {ToastContainer} from "react-toastify";
+import { persistor, store } from "./app/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GOOGLE_CLIENT_ID } from "./constants.ts";
 
-createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <CssBaseline/>
-        <ToastContainer/>
-        <App />
-    </BrowserRouter>
-)
+
+createRoot(document.getElementById("root")!).render(
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+            <ToastContainer/>
+            <App />
+            <CssBaseline/>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </GoogleOAuthProvider>,
+);
