8efea529cc760014fda4233d369c3e548711b8b7
Shefler Alexandr <sasha.shefler9@gmail.com>
2026-04-30 19:43:50 +0500

    написал главную страницу коктелей и сделал компонент карточки универсальный

--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -1,9 +1,37 @@
-
+import {useAppDispatch, useAppSelector} from "../../app/hooks.ts";
+import {getListOfCocktails, getLoadingAllCocktails} from "../../features/cocktails/cocktailsSelectors.ts";
+import {selectUser} from "../../features/users/usersSelectors.ts";
+import CardCocktail from "../../components/CardCocktail/CardCocktail.tsx";
+import {useEffect} from "react";
+import {getAllCocktails} from "../../features/cocktails/cocktailsThunks.ts";
+import {Box, LinearProgress, Typography} from "@mui/material";
 
 const Home = () => {
+    const dispatch = useAppDispatch();
+    const allCocktails = useAppSelector(getListOfCocktails);
+    const loading = useAppSelector(getLoadingAllCocktails);
+    const user = useAppSelector(selectUser);
+
+    useEffect(() => {
+        dispatch(getAllCocktails());
+    }, [dispatch]);
+
     return (
         <>
-            Home
+            {allCocktails.length === 0 && <Typography variant="h6" sx={{mt:3, textAlign: "center"}}>No Cocktails yet</Typography>}
+            {loading && (<LinearProgress />)}
+            {!loading && allCocktails.length > 0 && (
+                <Box sx={{ display: "flex", alignItems: "center", gap: "1rem", mt: 4 }}>
+                    {allCocktails.map((c) => (
+                        <CardCocktail
+                            key={c._id}
+                            cocktail={c}
+                            isOwner={!!user}
+                            isAdmin={user?.role === "admin"}
+                        />
+                    ))}
+                </Box>
+            )}
         </>
     );
 };
