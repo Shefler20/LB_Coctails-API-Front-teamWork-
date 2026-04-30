fb63e87fafa219c08ffc679b124808a32b984ac5
Shefler Alexandr <sasha.shefler9@gmail.com>
2026-04-30 20:36:03 +0500

    сделал лаяут приватый для залогиненых пользователей и админов

--- frontend/src/components/PrivateRoute/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,15 +1,25 @@
-import  {type PropsWithChildren} from "react";
-import * as React from "react";
+import {type PropsWithChildren} from "react";
+import {Navigate} from "react-router-dom";
+import {useAppSelector} from "../../app/hooks.ts";
+import {selectUser} from "../../features/users/usersSelectors.ts";
 
-interface Props extends PropsWithChildren {}
 
-const PrivateRoute: React.FC<Props> = ({children}) => {
-    return (
-        <>
-            PrivateRoute
-            {children}
-        </>
-    );
+interface Props extends PropsWithChildren{
+    isAllowed: boolean | null;
+}
+
+const PrivateRoute: React.FC<Props> = ({isAllowed, children}) => {
+    const user = useAppSelector(selectUser);
+    if (!user) {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (!isAllowed) {
+        return <Navigate to="/" replace />;
+    }
+
+
+    return <>{children}</>;
 };
 
 export default PrivateRoute;
